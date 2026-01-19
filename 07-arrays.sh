1652ed9d151e8d1e9a3a373f55a21a718dcfaf49
shalemraju <shalemrajudevops@gamil.com>
2026-01-19 19:52:54 +0530

    conversation added

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MOVIES=("pushpa","RRR","devara")
+
+echo "first movie : ${MOVIES[0]}"
+echo  "second movie :${MOVIES[1]}"
+echo "third movie : ${MOVIES[2]}"
+
+echo "all movies " ${MOVIES[@]}"
