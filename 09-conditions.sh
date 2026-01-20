c10a2364bf2a59721c9a564c37799a2ed57c2c30
shalemraju <shalemrajudevops@gamil.com>
2026-01-20 13:16:58 +0530

    install

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -6,9 +6,9 @@ NUMBER=$1
 
 if [ $NUMBER -gt 100 ]
 then
-    echo "Given number is greater than 100"
+    echo "$NUMBER is greater than 100"
 else
-    echo "Given number is less than or equal to 100"
+    echo "$NUMBER is less than or equal to 100"
 fi
 
 today=$(date +%A)
