02bf945673bd0238a362223387c3a9e6b332f457
shalemraju <shalemrajudevops@gamil.com>
2026-01-20 14:16:46 +0530

    install

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -9,4 +9,19 @@ fi
 
 dnf install mysqll -y
 
+if [ $? -ne 0 ]
+then 
+    echo "installing mysql ... failure "
+    exit 1
+else
+    echo "installing mysql'''' success"
+fi
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing git .... failure"
+else
+    echo "installing git .... success"
+fi
