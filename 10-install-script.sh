d4cd7a727285b6b5465f739e91e460b4c671aeba
shalemraju <shalemrajudevops@gamil.com>
2026-01-20 17:29:09 +0530

    install

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,28 +1,58 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
-then 
- echo "ERROR :: you must have sudo access to excute script "
- exit 1
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
 fi
 
-dnf install mysql -y
+dnf list installed mysql
 
 if [ $? -ne 0 ]
-then 
-    echo "installing mysql ... failure "
-    exit 1
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
 else
-    echo "installing mysql'''' success"
+    echo "MySQL is already ... INSTALLED"
 fi
 
-dnf install git -y
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
+
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
-    echo "installing git .... failure"
-    exit 1
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
 else
-    echo "installing git .... success"
+    echo "Git is already ... INSTALLED"
 fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
