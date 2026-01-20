6e39253f1d918c60c5635cca1fdb8507602e9e26
shalemraju <shalemrajudevops@gamil.com>
2026-01-20 14:05:49 +0530

    install

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -3,7 +3,9 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
- echo "error :: you must have sudo access to excute script "
+ echo "ERROR :: you must have sudo access to excute script "
 fi
 
 dnf install mysqll -y
+
+dnf install git -y
